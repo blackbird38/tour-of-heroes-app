56915898112975b6f6f879f141dd307b151ded12
Black Bird <blackbird38fr@gmail.com>
2020-01-31 00:51:49 +0100

    Routable HeroDetailComponent, extracting the id route parameter, getting the corresponding hero through the service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -35,4 +35,12 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES); /*returns an Observable<Hero[]> that emits a single value, the array of mock heroes.*/
   }
+
+/*Like getHeroes(), getHero() has an asynchronous signature. It returns a mock hero as an Observable,
+ using the RxJS of() function.*/
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
