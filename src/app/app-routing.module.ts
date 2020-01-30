1b908b0be6204c857061c59c73c078a86503a56b
Black Bird <blackbird38fr@gmail.com>
2020-01-31 00:34:11 +0100

    added a hero detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'; /*so the app can have routing functionality*/
 import { HeroesComponent } from './heroes/heroes.component'; /* will give the Router somewhere to go once the routes are configured*/
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 /*
   Routes tell the Router which view to display when a user clicks a link or pastes
@@ -15,7 +16,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent},
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent }
+  /*The colon (:) in the path indicates that :id is a placeholder for a specific hero id.*/
 ];
 
 /*The @NgModule metadata initializes the router and starts its listening
